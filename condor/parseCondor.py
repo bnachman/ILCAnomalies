a17091e5a895a29f13e8a1e16e77cf201aaf6d55
Julia Gonski <julia.gonski@cern.ch>
2021-08-10 16:12:47 -0400

    consolidating parse PFN and EVT

--- condor/parseCondor.py
+++ condor/parseCondor.py
@@ -8,14 +8,8 @@
 # also see this https://anbasile.github.io/posts/2017-06-25-jupyter-venv/
 import sys
 import numpy as np
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import glob
-#import energyflow as ef
-#from energyflow.archs import DNN
-#from energyflow.datasets import qg_jets
-#from energyflow.utils import data_split, remap_pids, to_categorical
-#from keras.models import Sequential
-#from keras.layers import Dense 
 import logging
 from eventHelper import *
 from datetime import datetime
@@ -32,18 +26,10 @@ def get_x_vec(jet1,jet2):
         return xVec
 
 #--------------------------- Parse text files
-def parse_file(file_object,startNum,endNum,filename):
+def parse_file(file_object,startNum,endNum,filename,runType):
     all_records = []
     mymeasuredenergy = []
-    ## Generate spherical sample
-    #sphereSample = np.array([sphericalGen(i) for i in range(5)])
-    #sphereEng = np.array([engFromVec(sphereSample[j]) for j in range(5)])
-    ## Choose sphere n points
-    #sphInd = 2
-    #spherePoints1 = sphereSample[sphInd]
-    #sphereEng1 = sphereEng[sphInd]
-    photonPs= []
-    jetPs = []
+    max_n_jets = 15
 
     count = 0
     for line in file_object:
@@ -51,7 +37,7 @@ def parse_file(file_object,startNum,endNum,filename):
           count += 1
           continue 
         if count > int(endNum): break
-        if count%100 == 0: print('Line '+str(count))
+        if count%1000 == 0: print('Line '+str(count))
 
         metadata = line.split("J")[0]
         eventinfo = line.split("J")[1]
@@ -65,7 +51,8 @@ def parse_file(file_object,startNum,endNum,filename):
         this_record['eventweight'] = eventweight #this is the event "weight".  Let's ignoreit for now (we will need it later).
         njets = int(len(jets.split())/11) #number of "jets" 
         nparticles  = int(len(particles.split())/5) #number of particles
-        if njets<2: continue
+        if 'evt' in runType:
+          if njets<2: continue
 
 
         #True collision quantities
@@ -84,14 +71,25 @@ def parse_file(file_object,startNum,endNum,filename):
         this_record['measuredphotonphi'] = float(metadata.split()[10]) #photon azimuthal angle
         this_record['metadata'] = metadata.split()
 
+        #Truth initial 4 vectors 
+        incomingelectron = TLorentzVector(0.0,0.0,0.0,0.0)
+        incomingpositron = TLorentzVector(0.0,0.0,0.0,0.0)
+        incomingelectron.SetPxPyPzE(0.0,0.0,500.0,500.0)
+        incomingpositron.SetPxPyPzE(0.0,0.0,-500.0,500.0)
+        outgoingPhoton = TLorentzVector(0.0,0.0,0.0,0.0)
+        outgoingPhoton.SetPtEtaPhiM(float(metadata.split()[8]), float(metadata.split()[9]), float(metadata.split()[10]), 0.0)
+        this_record['measuredsqrtshatwphoton'] = (incomingelectron+incomingpositron-outgoingPhoton).M()
+
 
         mymeasuredenergy+=[measuredcenterofmassenergy]
 
 
         this_record['njets'] = njets
-        jets_vec = []
-        jets = jets.split()
-  
+        # jets for PFN input
+        jets_list = jets.split()  
+        jets_vec = np.array(jets_list).astype('float')
+        jets_vec = np.reshape(jets_vec, (-1, 11))
+        jets_vec = jets_vec[:,1:] #omit index 0 
         for i in range(njets):
             jet = np.zeros(11)
             #order:
@@ -108,8 +106,12 @@ def parse_file(file_object,startNum,endNum,filename):
             # - 4th angular moment of jet radiation
             jet = jets[i*11:i*11+11]
             jets_vec+=[jet]
+        j_pad_length = max_n_jets - jets_vec.shape[0]
+        padded_jets = np.pad(jets_vec, ((0,j_pad_length),(0,0)))
+        assert padded_jets.shape == (max_n_jets, 10)
+        this_record['jets']=padded_jets
 
-        this_record['jets']=jets_vec
+        # event level variables
         if len(jets_vec)>1: logging.warning('measured X pt: '+ str(get_x_vec(jets_vec[0], jets_vec[1]).Pt()))
         this_record['leadingjetpT']= float(jets_vec[0][1]) if len(jets_vec)>0 else -1
         this_record['subleadingjetpT']= float(jets_vec[1][1]) if len(jets_vec)>1 else -1
@@ -127,19 +129,19 @@ def parse_file(file_object,startNum,endNum,filename):
 
         this_record['nparticles'] = nparticles
 
-        particles = particles.split()
-        particles_vec = []
-        for i in range(nparticles):
-            particle = np.zeros(5)
-            #order:
-            # - index
-            # - magnitude of momentum pT (units of GeV)
-            # - pseudorapidity (~polar angle - see e.g. https://en.wikipedia.org/wiki/Pseudorapidity)
-            # - azimuthal angle
-            # - particle identifier (https://pdg.lbl.gov/2006/reviews/pdf-files/montecarlo-web.pdf)
-            particle = particles[i*5:i*5+5]
-            particles_vec+=[particle]
-        this_record['particles'] = particles_vec
+        #particles = particles.split()
+        #particles_vec = []
+        #for i in range(nparticles):
+        #    particle = np.zeros(5)
+        #    #order:
+        #    # - index
+        #    # - magnitude of momentum pT (units of GeV)
+        #    # - pseudorapidity (~polar angle - see e.g. https://en.wikipedia.org/wiki/Pseudorapidity)
+        #    # - azimuthal angle
+        #    # - particle identifier (https://pdg.lbl.gov/2006/reviews/pdf-files/montecarlo-web.pdf)
+        #    particle = particles[i*5:i*5+5]
+        #    particles_vec+=[particle]
+        #this_record['particles'] = particles_vec
         
         isNan = False
         for ele in particles_vec:
@@ -152,13 +154,18 @@ def parse_file(file_object,startNum,endNum,filename):
         this_record['transverse_sphericity'] = transverse_sphericity(w,v)
         this_record['aplanarity'] = aplanarity(w,v)
 
-        # Event isotropy 
-        #this_record['evIsoSphere'] = evIsoSphere(particles_vec,spherePoints1,sphereEng1)
-         
         
         all_records.append(this_record)
 
     return all_records
+
+#-----------------------------------------------------------------------------------
+def make_pfn_arrays(these_records):
+    padded_jet_arrays = []
+    for i,record in enumerate(these_records):
+        padded_jet_arrays.append(record['jets'])
+    #print('jets shape:', np.shape(padded_jet_arrays))
+    return np.array(padded_jet_arrays)
 
 #-----------------------------------------------------------------------------------
 def make_evt_arrays(these_records):
@@ -178,15 +185,17 @@ if __name__ == "__main__":
   filename=sys.argv[2]
   startNum=sys.argv[3]
   endNum=sys.argv[4]
+  runType=sys.argv[5]
   logging.basicConfig(level=logging.ERROR)
 
   records = []
-  print('Running filename ', filename, ' from line ', startNum, ' to ', endNum)
-  #file = open('../'+str(filename))
+  print('Running filename ', filename, ' as ', runType, ' from line ', startNum, ' to ', endNum)
   file = open(str(filename))
-  records += parse_file(file,startNum,endNum,filename)
-  X = make_evt_arrays(records)
+  records += parse_file(file,startNum,endNum,filename,runType)
+  if 'evt' in runType: X = make_evt_arrays(records)
+  elif 'pfn' in runType: X_make_pfn_arrays(records)
   y = np.array([i['truthsqrtshat'] for i in records])
+  #y = np.array([i['measuredsqrtshatwphoton'] for i in records])
   #y = np.array([i['measuredsqrtshat'] for i in records])
   np.save(tag+"_X_"+filename.split('/')[-1].split('.')[0]+"_"+str(startNum)+"to"+str(endNum), X)
   np.save(tag+"_y_"+filename.split('/')[-1].split('.')[0]+"_"+str(startNum)+"to"+str(endNum), y)
